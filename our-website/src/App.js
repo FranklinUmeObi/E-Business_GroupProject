1d8f602c1fd8ffac85566707dc26c13465a98c27
FranklinUmeObi <franklinumeobi@gmail.com>
2020-10-13 17:49:38 +0100

    made a wireframe header component

--- our-website/src/App.js
+++ our-website/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Components/header'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
      <h2>This should display if working correctly</h2>
     </div>
   );
---
+++ our-website/src/Components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css';
+
+function header() {
+    return (
+        <div className="header">
+            <p>This is our header</p>
+        </div>
+    )
+}
+
+export default header

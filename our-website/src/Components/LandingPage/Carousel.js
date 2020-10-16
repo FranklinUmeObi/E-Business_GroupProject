95ead223feaa9aaa8d11af8539abf854b81e9738
FranklinUmeObi <franklinumeobi@gmail.com>
2020-10-16 21:23:38 +0100

    Added the carousel to the landing page and installed a package

---
+++ our-website/src/Components/LandingPage/Carousel.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Carousel as ReactCarousel  } from 'react-responsive-carousel';
+import "./Carousel.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img1 from "../../Assets/Carousel/1.png";
+import img2 from "../../Assets/Carousel/2.png";
+import img3 from "../../Assets/Carousel/3.png";
+
+function Carousel() {
+  return (
+    <div className="carouselContainer">
+         <ReactCarousel className="slider" showThumbs={false} showStatus={false} showArrows={true} autoPlay infiniteLoop>
+                <div>
+                    <img className="c_image" src={img1} alt="Slide1"/>
+                    <Link to="/">
+                        <button className="c_button">
+                            <h4 >Customise Your Perfect Christmas Sock</h4>
+                        </button>
+                    </Link>
+                </div>
+
+                <div>
+                    <img className="c_image" src={img2} alt="Slide2" />
+                    <Link to="/">
+                        <button className="c_button">
+                            <h4> Customise Your Perfect Christmas Sock</h4>
+                        </button>
+                    </Link>
+                </div>
+                <div>
+                    <img className="c_image" src={img3} alt="Slide3" />
+                    <Link to="/">
+                        <button className="c_button">
+                            <h4 >Customise Your Perfect Christmas Sock</h4>
+                        </button>
+                    </Link>
+                </div>
+            </ReactCarousel>
+    </div>
+  );
+}
+
+export default Carousel;
--- our-website/src/Components/LandingPage/LandingPage.js
+++ our-website/src/Components/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Carousel from      './Carousel'
 import './LandingPage.css';
 
 function LandingPage() {
     return (
         <div className="LandingPage">
-            
+             <div className="pageContainer">
+                 <h1>Customise your stocking today</h1>
+                 <Carousel/>
+                                  
+                 <h2>H</h2>
+                 <h2>H</h2>
+                 <h2>H</h2>
+                 <h2>H</h2>
+                 <h2>H</h2>
+             </div>
         </div>
     )
 }

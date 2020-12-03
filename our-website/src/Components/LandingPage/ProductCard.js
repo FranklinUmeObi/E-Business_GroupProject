fe3e9d2ea70b668e918740ac2efb27d11dd2eadd
Franklin Ume Obiekwe <franklinumeobi@gmail.com>
2020-12-03 18:31:46 +0000

    added reviews to product cards

--- our-website/src/Components/LandingPage/ProductCard.js
+++ our-website/src/Components/LandingPage/ProductCard.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, {useState} from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+import { IconButton } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 import "./ProductCard.css";
 
 
+
 function ProductCard(props) {
+  const [state, setState] = useState({
+    likes: Math.floor(Math.random() * 100)
+});
+const [stateColour, setStateColour] = useState({
+  heart: 'grey'
+});
+
+function addLike(){
+  let newCount = state.likes + 1;
+    setState({likes: newCount});
+    setStateColour({heart: 'red'});
+};
+
 
   return (
     <div className="card">
@@ -32,10 +52,40 @@ function ProductCard(props) {
             <h2 className="cardIconText">Add</h2>
           <FontAwesomeIcon icon={faShoppingCart} />
         </button>
+        
       </div>
 
-    </div>
-  );
-}
+      <div className="reviewsContainer">
+        <Popup trigger={<button className="stock"> <h2 className="stockText ">Reviews</h2> </button>}modal nested>
+          {close => (
+            <div className="popUp flip-diagonal-1-fwd">
+              <h2>{props.name}: Reviews</h2>
 
-export default ProductCard;
+              <div className="likeContainer">
+                <h4>Total likes: {state.likes}</h4>
+
+                <IconButton color="secondary" aria-label="Like" onClick={addLike}>
+                  <FavoriteIcon style={{ color: stateColour.heart }}/>
+                </IconButton>
+              </div>
+              
+              <div className="review">
+                <h3>Maggie Musk</h3>
+                <p>Fairly deece I must say. Kiddos are only raving over em</p>
+              </div>
+              <div className="review">
+                <h3>Lil Piddle</h3>
+                <p>Groovy Baby</p>
+              </div>
+
+                <button className="popButton" onClick={() => {close();}}>Close Reviews</button>
+
+            </div>
+          )}
+        </Popup>
+        </div>
+      </div>
+    );
+  }
+
+  export default ProductCard;
